75f8e54e2107bcce3df06a06368d45b20c24cc94
Athif-gitt <athiffaisal55@gmail.com>
2025-12-17 10:49:00 +0530

    ORM

---
+++ smsproject/orm_practice/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.CharField(max_length=50)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
--- smsproject/smsproject/settings.py
+++ smsproject/smsproject/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'students',
+    'students',
     'admins',
     'accounts',
-    'students.apps.StudentsConfig',
+    'orm_practice',
+    # 'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
